476d33fd6f3ba434cf2fce6451fd43bc8cdeffcb
Roundy <nroundy801@gmail.com>
2020-06-11 10:08:45 -0600

    added boiler for server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const massive = require("massive");
-// const session = require("express-session");
+const session = require("express-session");
 
-const { SERVER_PORT, CONNECTION_STRING } = process.env;
+const { SERVER_PORT, CONNECTION_STRING, SECRET } = process.env;
 
 
 // Controllers
@@ -15,16 +15,27 @@ const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: SECRET,
+    cookie: { secure: false }
+  })
+);
 
 //
 
+// app.listen(SERVER_PORT, () =>
+//   console.log(`${SERVER_PORT} barrels of beer`)
+// );
 
 massive(CONNECTION_STRING).then(database => {
   app.set("db", database);
   app.listen(SERVER_PORT, () =>
-    console.log(`listening on port ${SERVER_PORT}`)
+    console.log(`${SERVER_PORT}  barrels of beer`)
   );
 });
 
