8403a193528df2ad67f1d6eeca1a00204ce08584
Roundy <nroundy801@gmail.com>
2020-06-15 19:39:30 -0600

    server is live and running

--- server/server.js
+++ server/server.js
@@ -1,15 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const massive = require("massive");
-const session = require("express-session");
 
-const { SERVER_PORT, CONNECTION_STRING, SECRET } = process.env;
-
+const { CONNECTION_STRING } = process.env;
+const SERVER_PORT = process.env.SERVER_PORT || 8000;
 
 // Controllers
 
 //
-
 
 // Middleware
 
@@ -17,39 +15,15 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-  session({
-    resave: true,
-    saveUninitialized: true,
-    secret: SECRET,
-    cookie: { secure: false }
-  })
-);
-
-//
-
-// app.listen(SERVER_PORT, () =>
-//   console.log(`${SERVER_PORT} barrels of beer`)
-// );
-
-massive(CONNECTION_STRING).then(database => {
-  app.set("db", database);
-  app.listen(SERVER_PORT, () =>
-    console.log(`${SERVER_PORT}  barrels of beer`)
-  );
+massive({
+  connectionString: CONNECTION_STRING,
+  sslmode: "require",
+  ssl: { rejectUnauthorized: false },
+}).then((database) => {
+  app.set("db, database");
+  app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} barrels of beer`));
 });
 
-
-
-
-
-// ! use as Ref
-// const express = require('express');
-
-// const app = express();
-
-
-
-// app.listen(3000, function(){
-//   console.log("Listening")
-// });
+// ! type ====> http://localhost:4420/ into browser and you should see the text Hello World
+app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'))
+// !
