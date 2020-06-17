c526683930af3a6bdd0de908870f92e5d9fdccb1
Roundy <nroundy801@gmail.com>
2020-06-17 16:44:19 -0600

    alter style table and changed REF PK from style_origin to FK from style tables

--- db/seed.sql
+++ db/seed.sql
@@ -1,54 +1,36 @@
+--! BASE STYLE
+
 create table style (
-style_id SERIAL PRIMARY KEY,
+base_style_id SERIAL PRIMARY KEY,
 styles text
 
 );
 
-INSERT INTO style (styles) VALUES 
+INSERT INTO style (style) VALUES 
 ('Ale Styles'),
 ('Lager Styles'), 
 ('Hybrid Mixed Lagers or Ales');
 
 SELECT * FROM STYLE;
 
---! ORIGIN
--- *******
-create table ale_styles_origin (
-style_id SERIAL PRIMARY KEY,
+--! STYLE ORIGIN
+create table style_origin (
+style_origin_id SERIAL PRIMARY KEY,
 styles VARCHAR (100)
+style_id INTEGER REFERENCES style(base_style_id)
+
 );
 
--- ALTER TABLE Origin RENAME COLUMN style_id TO origin_id;
+-- DROP TABLE ale_styles
 
-INSERT INTO ale_styles_origin (ale_styles_origin) VALUES 
-('British Origin Ale Styles'),
-('North American Origin Ale Styles'),
-('Belgian and French Origin Ale Styles'),
-('Irish Origin Ale Styles'),
-('German Origin Ale Styles'),
-('Other Origin Ale Styles');
-SELECT * FROM ale_styles_origin;
--- *******
-
--- *******
-create table Lager_styles_origin(
-style_id SERIAL PRIMARY KEY,
-Lager_Styles_Origin VARCHAR (100)
-);
-INSERT INTO Lager_styles_origin (Lager_styles_origin) VALUES 
-('European Origin Lager Styles'),
-('North American Origin Lager Styles'),
-('Other Origin Lager Styles');
-SELECT * FROM lager_styles_origin;
--- ****
--- ****
-create table Hybrid_Mixed_Lager_Ale_Styles_Origin (
-style_id SERIAL PRIMARY KEY,
-Hybrid_Mixed_Lager_Ale_Styles_Origin VARCHAR (100)
-);
-INSERT INTO Hybrid_Mixed_Lager_Ale_Styles_Origin (Hybrid_Mixed_Lager_Ale_Styles_Origin) VALUES 
-('All Origin Hybrid/Mixed Lagers or Ale');
-SELECT * FROM Hybrid_Mixed_Lager_Ale_Styles_Origin;
--- ***END***
-
-
+INSERT INTO style_origin (styles, style_id) VALUES 
+('British Origin Ale Styles', 1),
+('North American Origin Ale Styles',1),
+('Belgian and French Origin Ale Styles',1),
+('Irish Origin Ale Styles',1),
+('German Origin Ale Styles',1),
+('Other Origin Ale Styles',1),
+('European Origin Lager Styles',2),
+('North American Origin Lager Styles',2),
+('Other Origin Lager Styles',2),
+('All Origin Hybrid/Mixed Lagers or Ale',3);
